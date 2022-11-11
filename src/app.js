bfae9c7a6e4d98f0973c5593bcb7bec9658a9e74
isabellexvr <isabellexavier59@gmail.com>
2022-11-11 15:49:50 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express, { json } from "express";
+import cors from "cors";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+  db = mongoClient.db("uolDatabase");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running in port 5000");
+});
