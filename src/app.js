593a95a7c94f0ac410d14a255699ce61365d3689
isabellexvr <isabellexavier59@gmail.com>
2022-11-11 16:07:15 -0300

    feat: data validation schema

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
+import joi from "joi";
 
 dotenv.config();
 
@@ -11,10 +12,23 @@ app.use(json());
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
-
 mongoClient.connect().then(() => {
   db = mongoClient.db("uolDatabase");
 });
+
+const userSchema = joi.object({
+  name: joi.string().required(),
+  lastStatus: joi.number().required(),
+});
+
+const messageSchema = joi.object({
+  from: joi.string().required(),
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required(),
+  time: joi.number().required,
+});
+
 
 app.listen(5000, () => {
   console.log("Server is running in port 5000");
