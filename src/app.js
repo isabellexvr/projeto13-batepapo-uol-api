c0aaf4b7363e42abb9b34a4e6f268f056f33acc5
isabellexvr <isabellexavier59@gmail.com>
2022-11-11 17:02:37 -0300

    feat: create a new user

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
+import dayjs from "dayjs";
 
+dayjs.locale("br");
 dotenv.config();
 
 const app = express();
@@ -16,11 +18,6 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("uolDatabase");
 });
 
-const userSchema = joi.object({
-  name: joi.string().required(),
-  lastStatus: joi.number().required(),
-});
-
 const messageSchema = joi.object({
   from: joi.string().required(),
   to: joi.string().required(),
@@ -29,6 +26,42 @@ const messageSchema = joi.object({
   time: joi.number().required,
 });
 
+app.post("/participants", async (req, res) => {
+  const promptSchema = joi.object({
+    name: joi.string().required(),
+  });
+  const validation = promptSchema.validate(req.body);
+  if (validation.error) {
+    res.sendStatus(422);
+    return;
+  }
+
+  const participantsCollection = db.collection("participants");
+  const messageCollection = db.collection("messages");
+
+  try {
+    const participants = await participantsCollection.find().toArray();
+    const isAlreadyRegistered = participants.find( e => req.body.name === e.name);
+    if (isAlreadyRegistered) {
+      res.sendStatus(409);
+      return;
+    }
+    await participantsCollection.insertOne({
+      name: req.body.name,
+      lastStatus: Date.now(),
+    });
+    await messageCollection.insertOne({
+      from: req.body.name,
+      to: "Todos",
+      text: "entra na sala..",
+      type: "status",
+      time: dayjs().format("HH:mm:ss"),
+    });
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(5000, () => {
   console.log("Server is running in port 5000");
