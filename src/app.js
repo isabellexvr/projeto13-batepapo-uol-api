9e1061c74eb87e7a533cf21f464765777155b5ba
isabellexvr <isabellexavier59@gmail.com>
2022-11-12 14:40:32 -0300

    feat: post route (post messages)

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import joi from "joi";
 import dayjs from "dayjs";
 
+//mandar objeto sem {}
+
 dayjs.locale("br");
 dotenv.config();
 
@@ -13,47 +15,44 @@ app.use(cors());
 app.use(json());
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-let db;
-mongoClient.connect().then(() => {
-  db = mongoClient.db("uolDatabase");
-});
 
-const messageSchema = joi.object({
-  from: joi.string().required(),
-  to: joi.string().required(),
-  text: joi.string().required(),
-  type: joi.string().required(),
-  time: joi.number().required,
-});
+try {
+  await mongoClient.connect();
+} catch (err) {
+  console.log(err);
+}
+
+const db = mongoClient.db("uolDatabase");
+const participantsCollection = db.collection("participants");
+const messageCollection = db.collection("messages");
 
 app.post("/participants", async (req, res) => {
   const promptSchema = joi.object({
     name: joi.string().required(),
   });
-  const validation = promptSchema.validate(req.body);
-  if (validation.error) {
+  const { error } = promptSchema.validate(req.body);
+  if (error) {
     res.sendStatus(422);
     return;
   }
 
-  const participantsCollection = db.collection("participants");
-  const messageCollection = db.collection("messages");
+  const username = req.body.name;
 
   try {
-    const participants = await participantsCollection.find().toArray();
-    const isAlreadyRegistered = participants.find(
-      (e) => req.body.name === e.name
-    );
-    if (isAlreadyRegistered) {
+    const participantExists = await participantsCollection.findOne({
+      name: username,
+    });
+
+    if (participantExists) {
       res.sendStatus(409);
       return;
     }
     await participantsCollection.insertOne({
-      name: req.body.name,
+      name: username,
       lastStatus: Date.now(),
     });
     await messageCollection.insertOne({
-      from: req.body.name,
+      from: username,
       to: "Todos",
       text: "entra na sala..",
       type: "status",
@@ -71,8 +70,41 @@ app.get("/participants", async (req, res) => {
     res.send(await participantsCollection.find().toArray());
   } catch (err) {
     console.log(err);
-    res.send("oi");
+    res.status(422).send("Não foi possível retornar a lista de participantes.");
   }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+
+  const { from } = req.headers;
+
+  const participantExists = await participantsCollection.findOne({name: from})
+
+  if (!participantExists){
+    return res.status(422).send("Participante não existe!");
+  }
+
+  const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid("message", "private_message").required()
+  });
+
+  const { error } = messageSchema.validate(req.body);
+
+  if (error) {
+    return res.status(422).send("Erro na composição da mensagem!");
+  }
+
+  await messageCollection.insertOne({
+    to,
+    text,
+    type,
+    from,
+    time: dayjs().format("HH:mm:ss"),
+  });
+  res.sendStatus(201);
 });
 
 app.listen(5000, () => {
